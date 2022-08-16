41382de4ed295922d0b541deac163e2a872e3c65
djebby <djebby.firas@gmail.com>
2022-08-16 13:52:57 +0100

    restaurant settings controller et models compléter

--- desktop-version/src/controllers/RestaurantSettingsController.java
+++ desktop-version/src/controllers/RestaurantSettingsController.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 import models.Meal;
 import models.Restaurant;
 import models.ServiceRestaurant;
@@ -17,6 +23,7 @@ public class RestaurantSettingsController {
 
     private Restaurant restaurant = null;
     private int restaurantManagerId = 1; // this value of the logged in restaurant manager will come later from the login form
+    private int mealIdToBeUpdated;
 
     @FXML
     private TextField restaurantNameTextField;
@@ -31,7 +38,13 @@ public class RestaurantSettingsController {
     @FXML
     private Spinner<Double> priceSpinner;
     @FXML
+    private Button addMealButton;
+    @FXML
+    private Button modifyMealButton;
+    @FXML
     private Label addMealMessageLabel;
+    @FXML
+    private VBox menuVBox;
 
     
 
@@ -41,20 +54,13 @@ public class RestaurantSettingsController {
 
     public void initialize() {
         try {
-            restaurant = serviceRestaurant.findByManagerId(this.restaurantManagerId);
-            this.restaurantNameTextField.setText(restaurant.getName());
-            this.restaurantAddressTextField.setText(restaurant.getAddress());
-            ArrayList<Meal> menu = serviceRestaurant.deserializeJSONMenu(restaurant.getMenu());
-
-            for (int i = 0; i < menu.size(); i++) {
-                System.out.println(menu.get(i).getName());
-            }
-
+            this.valueFactory.setValue(0.5);
+            this.priceSpinner.setValueFactory(valueFactory);
+            this.renderRestaurantMenu();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        this.valueFactory.setValue(0.5);
-        this.priceSpinner.setValueFactory(valueFactory);
+        
     }
 
 
@@ -80,6 +86,7 @@ public class RestaurantSettingsController {
             if(serviceRestaurant.addMealToMenu(this.restaurant.getId(), meal)) {
                 this.addMealMessageLabel.setText("Repas ajouter avec succés");
                 this.emptyAddMealField();
+                this.renderRestaurantMenu();
             }
             else this.addMealMessageLabel.setText("échec lors de l'ajout de repas");
                 
@@ -95,6 +102,8 @@ public class RestaurantSettingsController {
     public void fieldResetHandler() {
         this.emptyAddMealField();
         this.addMealMessageLabel.setText("");
+        this.addMealButton.setDisable(false);
+        this.modifyMealButton.setDisable(true);
         
     }
 
@@ -105,5 +114,87 @@ public class RestaurantSettingsController {
         this.valueFactory.setValue(0.5);
         this.priceSpinner.setValueFactory(this.valueFactory);
     }
+
+
+    public void renderRestaurantMenu() {
+        this.menuVBox.getChildren().clear();
+        
+        
+        try {
+            
+            restaurant = serviceRestaurant.findByManagerId(this.restaurantManagerId);
+            this.restaurantNameTextField.setText(restaurant.getName());
+            this.restaurantAddressTextField.setText(restaurant.getAddress());
+            ArrayList<Meal> menu = serviceRestaurant.deserializeJSONMenu(restaurant.getMenu());
+            for (int i = 0; i < menu.size(); i++) {
+                Meal meal = menu.get(i);
+                int index = i;
+
+                String mealRow = " " + meal.getName() + "    " + meal.getPrice() + " TND";
+
+               
+                Label label01 = new Label(mealRow); label01.setFont(new Font("Arial", 16));
+                Button modifyMealBtn = new Button(" MODIFIER ");
+                modifyMealBtn.setOnAction(event -> this.sendMealToModification(index, menu.get(index)));
+                Button deleteMealBtn = new Button(" SUPRIMER ");
+                deleteMealBtn.setOnAction(event -> { this.deleteMealHandler(index); });
+                FlowPane flowPane = new FlowPane( label01, modifyMealBtn, deleteMealBtn);
+                flowPane.setHgap(10);
+                Label label02 = new Label("     "+meal.getDescription()); label02.setFont(new Font("Arial", 16));
+                Separator separator = new Separator();
+                this.menuVBox.setSpacing(15.0);
+                this.menuVBox.getChildren().addAll(flowPane, label02, separator);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void sendMealToModification(int index, Meal meal) {
+        this.addMealButton.setDisable(true);
+        this.modifyMealButton.setDisable(false);
+        this.mealIdToBeUpdated = index;
+
+        this.mealsNameTextField.setText(meal.getName());
+        this.mealsDescripTextField.setText(meal.getDescription());
+        this.valueFactory.setValue(meal.getPrice());
+        this.priceSpinner.setValueFactory(this.valueFactory);
+    }
+
+    public void updateMeal() {
+        try {
+            boolean updateResult = this.serviceRestaurant.updateMeal(this.restaurant.getId(), this.mealIdToBeUpdated, this.mealsNameTextField.getText(), this.mealsDescripTextField.getText(), this.priceSpinner.getValue());
+            
+            if(updateResult) {
+                this.addMealMessageLabel.setText("Repas modifier avec succés");
+                this.emptyAddMealField();
+                this.addMealButton.setDisable(false);
+                this.modifyMealButton.setDisable(true);
+            } else {
+                this.addMealMessageLabel.setText("échec lors de la modification de repas");
+            }
+        } catch (SQLException e) {
+            this.addMealMessageLabel.setText("échec lors de la modification de repas");
+            System.out.println(e.getMessage());
+        }
+        this.renderRestaurantMenu();
+    }
+
+
+    public void deleteMealHandler(int mealIndex) {
+        try {
+            this.serviceRestaurant.deleteMeal(this.restaurant.getId(), mealIndex);
+            this.renderRestaurantMenu();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public void returnToCustomerOrderHandler(ActionEvent event) {
+        System.out.println("Return To Customer Order Button Clicked");
+    }
     
+
 }
--- desktop-version/src/models/ServiceRestaurant.java
+++ desktop-version/src/models/ServiceRestaurant.java
@@ -56,27 +56,68 @@ public class ServiceRestaurant implements IService<Restaurant> {
 
 
     public boolean addMealToMenu(int idRestaurant, Meal meal) throws SQLException {
-        
         this.statement = this.connection.createStatement();
         String request1 = "SET @json_menu = (SELECT menu FROM restaurants WHERE id = "+idRestaurant+")";
         String request2 = "UPDATE restaurants SET menu = (SELECT JSON_ARRAY_APPEND(@json_menu, '$', CAST('{\"name\": \""+meal.getName()+"\", \"price\": "
                             +meal.getPrice()+", \"description\":\""+meal.getDescription()+"\"}' AS JSON))) WHERE id = " + idRestaurant;
-
-
         this.statement.executeUpdate(request1);
         return this.statement.executeUpdate(request2) == 1;
     }
 
+
     public ArrayList<Meal> deserializeJSONMenu(String JSONMenu) throws Exception{
-
         ArrayList<Meal> menu = new ArrayList<>();
-
         ObjectMapper mapper = new ObjectMapper();
         Meal[] meals = mapper.readValue(JSONMenu, Meal[].class);
-
         for (int i = 0; i < meals.length; i++) menu.add(meals[i]);
-
         return menu;
+    }
+
+
+    public boolean updateMeal(int idRestaurant, int mealIndex, String newName, String newDescription, double newPrice) throws SQLException {
+        this.statement = this.connection.createStatement();
+        String request1 = "SET @json_column_value = ( SELECT menu FROM restaurants WHERE id = "+idRestaurant+")";
+        String request2 = "UPDATE restaurants SET menu = (SELECT JSON_SET(@json_column_value, '$["+mealIndex+"]', CAST('{\"name\": \""+newName+"\", \"price\": "
+                            +newPrice+", \"description\": \""+newDescription+"\" }' AS JSON))) WHERE id = " + idRestaurant;
+        
+        this.statement.executeUpdate(request1);
+        return this.statement.executeUpdate(request2) == 1;
+
+    }
+
+    public boolean deleteMeal(int idRestaurant, int mealIndex) throws SQLException {
+        this.statement = this.connection.createStatement();
+        String request1 = "SET @json_column_value = ( SELECT menu FROM restaurants WHERE id = "+idRestaurant+")";
+        String request2 = "UPDATE restaurants SET menu = (SELECT JSON_REMOVE(@json_column_value, '$["+mealIndex+"]')) WHERE id = " + idRestaurant;
+
+        this.statement.executeUpdate(request1);
+        return this.statement.executeUpdate(request2) == 1;
     }
     
 }
+
+
+/*
+* to add item in a json array column in table first fetch the old json state then update
+* SET @json_column_value = (SELECT <column_name> FROM <table_name> WHERE <condition>);
+* UPDATE <table_name> SET <column_name> = (SELECT JSON_ARRAY_APPEND(@json_column_value, '$',  CAST('<your_value>' AS JSON))) WHERE <condition>;
+* 
+* in our exemple : 
+* SET @json_column_value = (SELECT meals_json FROM persons WHERE id = 1);
+* UPDATE persons SET meals_json = (SELECT JSON_ARRAY_APPEND(@json_column_value, '$', CAST('<your_value>' AS JSON))) WHERE id = 1;
+*/
+
+/*
+ *
+ * to update item in json array column in table ==>>> 
+ * SET @json_column_value = ( SELECT <column_name> FROM <table_name> WHERE <condition> );
+ * UPDATE <table_name> SET <column_name> = (SELECT JSON_SET(@json_column_value, '$[index]', CAST('<your_new_value>' AS JSON))) WHERE <condition>;
+ * 
+ */
+
+/*
+ * to delete item in json array column in a table ==>>>
+ * SET @json_column_value = ( SELECT <column_name> FROM <table_name> WHERE <condition> );
+ * UPDATE <table_name> SET <column_name> = (SELECT JSON_REMOVE(@json_column_value, '$[index]')) WHERE <condition>;
+ * 
+ */
