37454a38fd7abb82ae42c89bdbcbc6b4c99c9526
Aouichichaima <chaima.aouichi@esprit.tn>
2022-09-19 15:27:25 +0200

    améloration des interface...

---
+++ desktop-version/src/controllers/AvisController.java
@@ -0,0 +1,38 @@
+package controllers;
+ 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class AvisController {
+
+    @FXML
+    private Text Text;
+
+    @FXML
+    private Label Label;
+
+    @FXML
+    private Button DonnerMonAvis;
+
+    @FXML
+    private Label Labell ;
+    /**
+     * @param event
+     */
+    @FXML
+    public void Avis(ActionEvent event) {
+
+    
+
+
+    button.setOnAction(new EventHandler<ActionEvent>() {
+
+    @Override
+    public void handle(ActionEvent event) {
+        button.setText(new AvisController().toString());
+    
+
